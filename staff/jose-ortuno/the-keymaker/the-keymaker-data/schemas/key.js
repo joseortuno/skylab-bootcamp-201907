74ff1e461ad8fd9a275a21c7aefad50f360a8d8a
joseortuno <joseantortunorioja@gmail.com>
2019-09-05 17:08:51 +0200

    update schemas

--- staff/jose-ortuno/the-keymaker/the-keymaker-data/schemas/key.js
+++ staff/jose-ortuno/the-keymaker/the-keymaker-data/schemas/key.js
@@ -29,6 +29,16 @@ module.exports = new Schema ({
         type: String,
         required: true
     },
+    alias_guest: {
+        type: String,
+        required: true,
+    },
+    email_guest: {
+        type: String,
+        required: true,
+        lowercase: true,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    },
     depolyment: [
         { type: ObjectId, ref: 'Deployment' }
     ],
