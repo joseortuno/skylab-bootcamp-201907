a0bf9c0c8cb24bb6fd8434f1c9f6b18212cc9a4f
joseortuno <joseantortunorioja@gmail.com>
2019-09-05 16:36:08 +0200

    edit validate

--- staff/jose-ortuno/the-keymaker/the-keymaker-utils/validate/index.spec.js
+++ staff/jose-ortuno/the-keymaker/the-keymaker-utils/validate/index.spec.js
@@ -15,14 +15,13 @@ describe("validate", () => {
         })
     })
 
-
     describe("email", () => {
         it("should detect that it's a email and not throw error", () => {
             const response = validate.email("a.a@test.com", "email")
             expect(response).to.be.undefined
         })
         it("should throw error if the email is not a email", () => {
-            expect(() => { validate.email("a.a@com","email") }).to.throw(Error, "email with value a.a@com is not a valid e-mail")
+            expect(() => { validate.email("a.a@com", "email") }).to.throw(Error, "email with value a.a@com is not a valid e-mail")
         })
     })
 
@@ -46,4 +45,50 @@ describe("validate", () => {
             expect(() => { validate.url("hola", "url") }).to.throw("url with value hola is not a valid URL")
         })
     })
+
+    describe("url", () => {
+        it("should detect that it's a url and not throw error", () => {
+            const response = validate.url("www.url.com", "url")
+            expect(response).to.be.undefined
+        })
+        it("should throw error if its not a url ", () => {
+            expect(() => { validate.url("hola", "url") }).to.throw("url with value hola is not a valid URL")
+        })
+    })
+
+    describe('Boolean', () => {
+        it('should succeed on correct Boolean', () => {
+            const result = validate.boolean(true, 'boolean')
+            expect(result).to.not.exist
+        })
+        it('should fail on non correct Boolean', () => {
+            expect(() =>
+                validate.boolean('true', 'boolean')).to.throw(Error, `boolean with value true is not a boolean`)
+        })
+    })
+
+    describe('Number', () => {
+        it('should succeed on correct Number', () => {
+            const result = validate.number(123, 'number')
+            expect(result).to.not.exist
+        })
+        it('should fail on non correct Number', () => {
+            expect(() =>
+                validate.number('123', 'number')).to.throw(Error, `number with value 123 is not a number`)
+        })
+    })
+
+    describe('Date', () => {
+        it('should succeed on correct Date', () => {
+            const newDate = new Date()
+            const result = validate.date(newDate, 'date')
+            expect(result).to.not.exist
+        })
+        it('should fail on non correct Date', () => {
+            const _newDate = 'Thu Sep 05 2019 13:16:53 GMT+0200'
+            expect(() =>
+                validate.date(_newDate, 'date')).to.throw(Error, 'date with value Thu Sep 05 2019 13:16:53 GMT+0200 is not a date')
+        })
+    })
+
 })
