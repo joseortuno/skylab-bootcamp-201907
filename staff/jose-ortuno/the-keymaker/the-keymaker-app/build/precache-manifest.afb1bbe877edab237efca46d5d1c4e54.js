fbda9163db446c92b7ac8c0ffa07026d8ac10503
joseortuno <joseantortunorioja@gmail.com>
2019-09-23 17:08:25 +0200

    update package json

---
+++ staff/jose-ortuno/the-keymaker/the-keymaker-app/build/precache-manifest.afb1bbe877edab237efca46d5d1c4e54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3063eb1cc88029eebb3c0e1881e539d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a45358dac0d86ba8fc4",
+    "url": "/static/css/main.631440f6.chunk.css"
+  },
+  {
+    "revision": "fbe8b4f54bc6d6cd931a",
+    "url": "/static/js/2.384bac31.chunk.js"
+  },
+  {
+    "revision": "4a45358dac0d86ba8fc4",
+    "url": "/static/js/main.07038d2e.chunk.js"
+  },
+  {
+    "revision": "900e97c7ac12cffe3cef",
+    "url": "/static/js/runtime~main.5441ea08.js"
+  }
+]);
